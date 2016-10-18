5a9e8dd6c0952633bd0d535120cb8717e6836934
juaPenagos <juanpenagoss@hotmail.com>
2016-10-18 08:44:51 -0500

    Prueba de inicio

---
+++ VMarket/VMarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VMarket.Startup))]
+namespace VMarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
